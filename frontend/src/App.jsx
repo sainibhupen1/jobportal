62bcbdded920d616f744004716a87017cca32cf8
Bhupendra <sainibhupendra50042@gmail.com>
2024-09-20 13:49:53 +0530

    app.jsx file in something wrong

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./components/shared/Navbar";
 import Login from "./components/auth/Login";
 import Signup from "./components/auth/Signup";
 import Home from "./components/Home";
-import Jobs from "./components/jobs";
+import Jobs from "./components/Jobs";
 import Browse from "./components/Browse";
 import Profile from "./components/Profile";
 import JobDescription from "./components/JobDescription";
