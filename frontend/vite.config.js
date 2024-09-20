f5a88c3813df69818964141de92a1abc9040eacf
Bhupendra <sainibhupendra50042@gmail.com>
2024-09-20 14:18:49 +0530

    code missing

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,35 +1,36 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import { resolve } from 'path'
-
-// export default defineConfig({
-//   plugins: [react()],
-//   resolve: {
-//     alias: {
-//       '@': resolve(__dirname, 'src'), 
-//     },
-//   },
-
-
-
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
-
-  build: {
-    chunkSizeWarningLimit: 5000,
-
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
-        },
-      },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
     },
   },
 });
+
+
+
+// import { defineConfig } from 'vite';
+
+// export default defineConfig({
+
+//   build: {
+//     chunkSizeWarningLimit: 5000,
+
+//     rollupOptions: {
+//       output: {
+//         manualChunks(id) {
+//           if (id.includes('node_modules')) {
+//             return 'vendor';
+//           }
+//         },
+//       },
+//     },
+//   },
+// });
 
 
 
