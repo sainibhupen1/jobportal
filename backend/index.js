04fc28a3e1a3ee66fc7d965389acc41963053414
Bhupendra <sainibhupendra50042@gmail.com>
2024-09-20 14:29:19 +0530

    url change

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: 'https://job-portal-8wsk.onrender.com/',
     credentials: true
 }
 app.use(cors(corsOptions));
