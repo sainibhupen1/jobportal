e63692d55df6c5e4cdd06d4688a4e5e407b96640
Bhupendra <sainibhupendra50042@gmail.com>
2024-09-21 11:19:35 +0530

    applicant file change

--- frontend/src/components/admin/Applicants.jsx
+++ frontend/src/components/admin/Applicants.jsx
@@ -11,7 +11,7 @@ const Applicants = () => {
   const Params = useParams();
   const dispatch = useDispatch();
   const { applicants } = useSelector((store) => store.application);
-  console.log(applicants);
+  // console.log(applicants);
 
   useEffect(() => {
     const fetchAllApplicants = async () => {
@@ -34,7 +34,8 @@ const Applicants = () => {
       <Navbar />
       <div className="max-w-7xl mx-auto">
         <h1 className="font-bold text-xl my-5">
-          Applicants ({applicants.applications.length})
+          Your Applicants
+          {/* ({applicants.applications.length}) */}
         </h1>
         <ApplicantsTable />
       </div>
